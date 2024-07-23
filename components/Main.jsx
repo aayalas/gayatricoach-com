3b06db0962c44348c90bbf3ccf4de5df5237479d
Alex Ayala <schmitt.mx@gmail.com>
2024-07-23 11:21:12 -0600

    Last Changes Requested by GAS- updated

--- components/Main.jsx
+++ components/Main.jsx
@@ -32,25 +32,25 @@ function Main() {
       <section id="services" className="">
 				<div className="max-w-2xl mx-auto text-center py-16">						
 					<h2 className="text-3xl font-normal leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
-							{t('links.services.title')}
+						{t('links.services.title')}
 					</h2>
 					<p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">{t('links.services.subtitle')}</p>
 					<a href="#letstalk" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-              {t('links.services.button1')}   
+              			{t('links.services.button1')}   
 					</a>
 					<a href="#faqs" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-              {t('links.services.button2')}
+						{t('links.services.button2')}
 					</a> 
 				</div>
 				<div className="py-4 px-2 mx-auto max-w-screen-xl lg:py-4">
 					<div className="grid md:grid-cols-2 gap-8">
 						<div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-12 shadow-lg">
 							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
-                {t('links.services.tcard1')}
-              </h2>
+                				{t('links.services.tcard1')}
+              				</h2>
 							<p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
-                {t('links.services.pcard1')}
-              </p>
+								{t('links.services.pcard1')}
+              				</p>
 						</div>
 						<div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-12 shadow-lg">
 							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
@@ -60,6 +60,38 @@ function Main() {
 								{t('links.services.pcard2')}
 							</p>
 						</div>
+						{/* <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-12 shadow-lg">
+							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
+                				{t('links.services.tcard3')}
+              				</h2>
+							<p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+								{t('links.services.pcard3')}
+              				</p>
+						</div> */}
+						{/* <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-12 shadow-lg">
+							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
+								{t('links.services.tcard4')}
+							</h2>
+							<p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+								{t('links.services.pcard4')}
+							</p>
+						</div> */}
+						{/* <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-12 shadow-lg">
+							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
+                				{t('links.services.tcard5')}
+              				</h2>
+							<p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+								{t('links.services.pcard5')}
+              				</p>
+						</div> */}
+						{/* <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-4 md:p-12 shadow-lg">
+							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
+								{t('links.services.tcard6')}
+							</h2>
+							<p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-4">
+								{t('links.services.pcard6')}
+							</p>
+						</div> */}
 						{/* <div className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-6 md:p-12 shadow-lg">
 							<h2 className="text-gray-900 dark:text-white text-3xl font-normal mb-2">
                 {t('links.services.tcard3')}
@@ -105,6 +137,7 @@ function Main() {
 										<li>{t('links.about.p6')}</li>
 										<li>{t('links.about.p7')}</li>
 										<li>{t('links.about.p8')}</li>
+										<li>{t('links.about.p9')}</li>
 									</ul>
 								</div>
 						</div>
